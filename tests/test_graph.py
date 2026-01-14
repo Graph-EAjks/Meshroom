c0542f84755c3ae790daaf1895cfc0a02c2ba81d
Alice Sonolet <alice.sonolet@gmail.com>
2026-01-14 15:28:42 +0100

    [tests] Add tests and more protection for the rename in core/graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -285,3 +285,27 @@ def test_cyclic_connection_should_raise_error():
     # Then
     with pytest.raises(CyclicDependencyError):
         tC.output.connectTo(tB.input)
+
+
+def test_rename_nodes():
+    """
+    Test renaming nodes.
+    """
+
+    graph = Graph("")
+    ls0 = graph.addNewNode("Ls")
+    ls1 = graph.addNewNode("Ls")
+    ls2 = graph.addNewNode("Ls")
+    
+    # Test with empty string
+    assert ls0.name == "Ls_1"
+    graph.renameNode(ls0, "")
+    assert ls0.name == "Ls_1"
+    
+    # Rename
+    graph.renameNode(ls0, "nodels")
+    assert ls0.name == "nodels"
+    graph.renameNode(ls1, "nodels")
+    assert ls1.name == "nodels_1"
+    graph.renameNode(ls2, "nodels")
+    assert ls2.name == "nodels_2"
