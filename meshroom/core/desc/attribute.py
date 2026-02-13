f0cfdd439efba57f8df6a292478bf92fb4e95e69
Fabien Servant <fabien.servant@technicolor.com>
2026-02-13 14:34:56 +0100

    Enable output list attributes in the graph

--- meshroom/core/desc/attribute.py
+++ meshroom/core/desc/attribute.py
@@ -135,13 +135,15 @@ class Attribute(BaseObject):
 class ListAttribute(Attribute):
     """ A list of Attributes """
     def __init__(self, elementDesc, name, label, description, group="allParams", advanced=False, semantic="",
-                 enabled=True, joinChar=" ", visible=True, exposed=False):
+                 enabled=True, joinChar=" ", visible=True, exposed=False, value=[]):
         """
         :param elementDesc: the Attribute description of elements to store in that list
+        :param value: default value. Use None to declare a dynamic output ListAttribute
+                      whose content is set during processChunk.
         """
         self._elementDesc = elementDesc
         self._joinChar = joinChar
-        super(ListAttribute, self).__init__(name=name, label=label, description=description, value=[],
+        super(ListAttribute, self).__init__(name=name, label=label, description=description, value=value,
                                             invalidate=False, group=group, advanced=advanced, semantic=semantic,
                                             enabled=enabled, visible=visible, exposed=exposed)
 
